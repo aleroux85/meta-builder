22a686c03525aaf8cd4d7ee5db4dee2f055e4044
Anro le Roux <anro@oligoden.com>
2019-03-21 21:57:42 +0200

    Move PrjData to project.go

--- branches.go
+++ branches.go
@@ -1,30 +1,5 @@
 package builder
 
-type PrjData struct {
-	Prj *Project
-	FSF *FSFile
-}
-
-func (d PrjData) Files() map[string]*FSDirectory {
-	return d.Prj.Directories
-}
-
-func (d *PrjData) SetFile(file *FSFile) {
-	d.FSF = file
-}
-
-func (d PrjData) File() *FSFile {
-	return d.FSF
-}
-
-func (d PrjData) Project() *Project {
-	return d.Prj
-}
-
-type BranchSetter interface {
-	SetBranch(...DataBranch) DataBranch
-}
-
 func buildBranch(m BranchSetter) {
 	stepper := m.(BackRef)
 
--- project.go
+++ project.go
@@ -155,3 +155,28 @@ func (p *Project) LoadSecrets(fn string) {
 		p.Secrets = append(p.Secrets, secrets[i])
 	}
 }
+
+type PrjData struct {
+	Prj *Project
+	FSF *FSFile
+}
+
+func (d PrjData) Files() map[string]*FSDirectory {
+	return d.Prj.Directories
+}
+
+func (d *PrjData) SetFile(file *FSFile) {
+	d.FSF = file
+}
+
+func (d PrjData) File() *FSFile {
+	return d.FSF
+}
+
+func (d PrjData) Project() *Project {
+	return d.Prj
+}
+
+type BranchSetter interface {
+	SetBranch(...DataBranch) DataBranch
+}
